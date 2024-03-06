259db8d718caa9a991d2c2c7d6c5b09d4b77f99d
Aurelie Moustardier <naoorelie@gmail.com>
2024-03-06 20:53:27 +0100

    avancé application

--- script.js
+++ script.js
@@ -1,5 +1,104 @@
 
-//je veux que quand 
+const APIKey = '0eee8175d5eda1d78eee24d6db88d7d6';
+let ville = document.getElementById("ville");
 
-let ville = document.getElementById('ville')
-let BtnValiderVille = document.getElementById('')
+async function AppelerMeteo(input) {
+    const reponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${APIKey}&lang=fr&units=metric`)
+    const meteo = await reponse.json()
+    console.log(meteo)
+
+    //endroit ou je stocke l'infos
+    const meteoElement = document.getElementById("afficher-meteo")
+
+    //créer un texte/titre nom de ville
+    let nomVille = document.createElement("h2")
+    nomVille.innerText = meteo.name;
+    meteoElement.appendChild(nomVille);
+
+    //créer temps
+    let temperature = document.createElement("h3")
+    temperature.innerText = `${meteo.main.temp} °C`;
+    meteoElement.appendChild(temperature);
+
+    //image de la météo
+    let imageMeteo = document.createElement("img")
+    meteoElement.appendChild(imageMeteo);
+
+    switch (meteo.weather[0].main) {
+        case "Clouds": 
+            imageMeteo.src = 'images/nuageux.png';
+            break;
+
+        case "Rain":
+            imageMeteo.src = 'images/pluie.png';
+            break;
+        
+        case "Snow":
+            imageMeteo.src = 'images/neige.png';
+            break;
+        
+        case "Thunderstorm":
+            imageMeteo.src = 'images/orage.png';
+            break;
+        
+        case "Clear":
+            imageMeteo.src = 'images/soleil.png';
+            break;
+        
+        case "Drizzle":
+            imageMeteo.src = 'images/brouillard.png';
+            break;
+        case "Mist":
+            imageMeteo.src = 'images/brouillard.png';
+            break;
+        case "Smoke":
+            imageMeteo.src = 'images/brouillard.png';
+            break;
+        case "Haze":
+            imageMeteo.src = 'images/brouillard.png';
+            break;
+        case "Dust":
+            imageMeteo.src = 'images/brouillard.png';
+            break;
+        case "Fog":
+            imageMeteo.src = 'images/brouillard.png';
+            break;
+        case "Sand":
+            imageMeteo.src = 'images/brouillard.png';
+            break;
+        case "Ash":
+            imageMeteo.src = 'images/brouillard.png';
+            break;
+        case "Squall":
+            imageMeteo.src = 'images/brouillard.png';
+            break;
+        case "Tornado":
+            imageMeteo.src = 'images/brouillard.png';
+            break;
+    }
+
+    //description de la météo
+    let informationMeteo = document.createElement("p")
+    informationMeteo.innerText = meteo.weather[0].description;
+    meteoElement.appendChild(informationMeteo);
+
+    //endroit ou je stocke l'infos
+    const meteoHumidite= document.getElementById("humidite")
+
+    //humidite
+    let humidite = document.createElement("p")
+    humidite.innerText = `${meteo.main.humidity} %`;
+    meteoHumidite.appendChild(humidite);
+
+    //endroit ou je stocke l'infos
+    const meteoVent= document.getElementById("vent")
+
+    //vent
+    let vent = document.createElement("p")
+    vent.innerText = `${meteo.wind.speed} km/h`;
+    meteoVent.appendChild(vent);
+};
+
+
+
+
